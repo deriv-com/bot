bad04674a04d18aacb12830894c1d7d723f5488b
shafin-deriv <shafin@regentmarkets.com>
2024-05-15 11:41:08 +0800

    chore: add basic ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -8,7 +9,7 @@ export default defineConfig({
     define: {
         global: 'window',
     },
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), basicSsl()],
     server: {
         port: 8443,
     },
