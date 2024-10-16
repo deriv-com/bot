95028470d9205873b173bd4d8382399db4d93284
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2024-10-16 15:48:33 +0300

    chore: add announcement feature

---
+++ src/components/shared_ui/open-livechat-link/open-livechat-link.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import clsx from 'clsx';
+import { Localize } from '@deriv-com/translations';
+import Text from '../text';
+import './open-livechat-link.scss';
+
+type TOpenLiveChatLink = {
+    text_size?: React.ComponentProps<typeof Text>['size'];
+    className?: string;
+};
+
+const OpenLiveChatLink = ({ children, text_size, className }: React.PropsWithChildren<TOpenLiveChatLink>) => (
+    <button
+        type='button'
+        className={clsx('open-livechat__link', className)}
+        onClick={() => window.LC_API.open_chat_window()}
+    >
+        <Text size={text_size || 'xs'} weight='bold' color='brand-red-coral'>
+            {children || <Localize i18n_default_text='live chat' />}
+        </Text>
+    </button>
+);
+
+export default OpenLiveChatLink;
