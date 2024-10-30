572db8db3abea06c545130c535cad4ff2cc94b0c
Farabi <farabi@deriv.com>
2024-10-31 00:25:25 +0800

    fix: added new blocks and updated accumulator changes

--- src/stores/app-store.ts
+++ src/stores/app-store.ts
@@ -238,6 +238,7 @@ export default class AppStore {
                         b =>
                             b.type === 'trade_definition_tradeoptions' ||
                             b.type === 'trade_definition_multiplier' ||
+                            b.type === 'trade_definition_accumulator' ||
                             (b.isDescendantOf('trade_definition_multiplier') && b.category_ === 'trade_parameters')
                     );
 
