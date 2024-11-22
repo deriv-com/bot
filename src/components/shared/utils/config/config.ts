24399df9ea21d5e5173dfc8aa32f441e6d7ed968
Rupato <rupato@deriv.com>
2024-11-22 13:59:00 +0800

    fix: name casing

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -145,8 +145,8 @@ export const generateOAuthURL = () => {
     const configured_server_url = (LocalStorageUtils.getValue(LocalStorageConstants.configServerURL) ||
         original_url.hostname) as string;
 
-    const validServerURLs = ['green.derivws.com', 'red.derivws.com', 'blue.derivws.com'];
-    if (!validServerURLs.includes(configured_server_url)) {
+    const valid_server_urls = ['green.derivws.com', 'red.derivws.com', 'blue.derivws.com'];
+    if (!valid_server_urls.includes(configured_server_url)) {
         original_url.hostname = configured_server_url;
     }
     return original_url.toString() || oauth_url;
