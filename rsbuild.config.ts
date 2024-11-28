460314821a561894920ed31e766b84a9a5ed5fe3
Rupato <rupato@deriv.com>
2024-11-28 18:21:45 +0800

    fix: for charts icons

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -49,6 +49,7 @@ export default defineConfig({
             },
             { from: 'node_modules/@deriv/deriv-charts/dist/chart/assets/*', to: 'assets/[name][ext]' },
             { from: 'node_modules/@deriv/deriv-charts/dist/chart/assets/fonts/*', to: 'assets/fonts/[name][ext]' },
+            { from: 'node_modules/@deriv/deriv-charts/dist/chart/assets/shaders/*', to: 'assets/shaders/[name][ext]' },
             { from: path.join(__dirname, 'public') },
         ],
     },
