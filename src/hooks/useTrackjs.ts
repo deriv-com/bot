eecb5958266622c226a3d7e8aedbf5f51527f376
shafin-deriv <shafin@regentmarkets.com>
2024-12-17 19:11:14 +0800

    fix: add version for trackjs

--- src/hooks/useTrackjs.ts
+++ src/hooks/useTrackjs.ts
@@ -8,6 +8,8 @@ const { TRACKJS_TOKEN } = process.env;
  */
 const useTrackjs = () => {
     const isProduction = process.env.APP_ENV === 'production';
+    const trackjs_version = process.env.REF_NAME ?? 'undefined';
+
     const initTrackJS = (loginid: string) => {
         try {
             if (!TrackJS.isInstalled()) {
@@ -18,9 +20,7 @@ const useTrackjs = () => {
                     token: TRACKJS_TOKEN!,
                     userId: loginid,
                     version:
-                        (document.querySelector('meta[name=version]') as HTMLMetaElement)?.content ??
-                        process?.env?.REF_NAME ??
-                        'undefined',
+                        (document.querySelector('meta[name=version]') as HTMLMetaElement)?.content ?? trackjs_version,
                 });
             }
         } catch (error) {
