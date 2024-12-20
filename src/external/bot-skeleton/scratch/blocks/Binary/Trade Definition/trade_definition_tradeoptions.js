4e9ce9f47a98faaa097b43f458d174de0830bfec
Rupato Braganza <rupato@deriv.com>
2024-12-20 14:29:08 +0800

    fix: validation on blocks

--- src/external/bot-skeleton/scratch/blocks/Binary/Trade Definition/trade_definition_tradeoptions.js
+++ src/external/bot-skeleton/scratch/blocks/Binary/Trade Definition/trade_definition_tradeoptions.js
@@ -84,6 +84,11 @@ window.Blockly.Blocks.trade_definition_tradeoptions = {
                         const new_value = value.includes('.') ? parseFloat(`${value}`) : parseInt(`${value}`);
                         input_target.setFieldValue(new_value.toString(), 'NUM');
                     }
+                    if (input.name === 'AMOUNT') {
+                        this.validateAmount(value);
+                    } else if (input.name === 'DURATION') {
+                        this.validateDuration(value);
+                    }
                 });
         }
 
@@ -535,57 +540,85 @@ window.Blockly.Blocks.trade_definition_tradeoptions = {
         return container;
     },
     restricted_parents: ['trade_definition'],
+    validateAmount(input) {
+        const input_number = Number(input);
+        const max_payout = this.amount_limits?.max_payout;
+        const min_stake = this.amount_limits?.min_stake;
+        const highlight_class = 'block--error-highlighted';
+        if (min_stake && input_number < min_stake) {
+            if (!this.error_message) {
+                this.error_message = localize("Please enter a stake amount that's at least {{min_stake}}.", {
+                    min_stake,
+                });
+                this.svgGroup_?.classList?.add(highlight_class);
+            } else {
+                this.error_message = '';
+                this.svgGroup_?.classList?.remove(highlight_class);
+            }
+            return input_number < min_stake;
+        }
+        if (input_number > max_payout) {
+            if (!this.error_message) {
+                this.error_message = localize("Please enter a payout amount that's lower than {{max_payout}}.", {
+                    max_payout,
+                });
+                this.svgGroup_?.classList?.add(highlight_class);
+            } else {
+                this.error_message = '';
+                this.svgGroup_?.classList?.remove(highlight_class);
+            }
+            return input_number > max_payout;
+        }
+        this.error_message = localize('Amount must be a positive number.');
+        return !isNaN(input_number) && input_number <= 0;
+    },
+    validateDuration(input) {
+        const input_number = Number(input);
+        if (isNaN(input_number) || !this.durations.length) {
+            return false;
+        }
+
+        const duration = this.durations.find(d => d.unit === this.selected_duration);
+        const highlight_class = 'block--error-highlighted';
+        if (duration) {
+            const { min, max } = duration;
+            const is_valid_duration = input_number >= min && input_number <= max;
+            if (min === max) {
+                if (!this.error_message) {
+                    this.error_message = localize(
+                        'Duration value is not allowed. To run the bot, please enter {{min}}.',
+                        { min }
+                    );
+                    this.svgGroup_?.classList?.add(highlight_class);
+                } else {
+                    this.error_message = '';
+                    this.svgGroup_?.classList?.remove(highlight_class);
+                }
+            } else if (input_number < min || input_number > max) {
+                if (!this.error_message) {
+                    this.error_message = localize(
+                        'Duration value is not allowed. To run the bot, please enter a value between {{min}} to {{max}}.',
+                        { min, max }
+                    );
+                    this.svgGroup_?.classList?.add(highlight_class);
+                } else {
+                    this.error_message = '';
+                    this.svgGroup_.classList?.remove(highlight_class);
+                }
+            } else {
+                this.error_message = '';
+                this.svgGroup_?.classList?.remove(highlight_class);
+            }
+
+            return !is_valid_duration;
+        }
+
+        return false;
+    },
     getRequiredValueInputs() {
         return {
-            AMOUNT: input => {
-                const input_number = Number(input);
-                const max_payout = this.amount_limits?.max_payout;
-                const min_stake = this.amount_limits?.min_stake;
-                if (min_stake && input_number < min_stake) {
-                    this.error_message = localize("Please enter a stake amount that's at least {{min_stake}}.", {
-                        min_stake,
-                    });
-                    return input_number < min_stake;
-                }
-                if (max_payout && input_number > max_payout) {
-                    this.error_message = localize("Please enter a payout amount that's lower than {{max_payout}}.", {
-                        max_payout,
-                    });
-                    return input_number > max_payout;
-                }
-                this.error_message = localize('Amount must be a positive number.');
-                return !isNaN(input_number) && input_number <= 0;
-            },
-            DURATION: input => {
-                const input_number = Number(input);
-
-                if (isNaN(input_number) || !this.durations.length) {
-                    return false;
-                }
-
-                const duration = this.durations.find(d => d.unit === this.selected_duration);
-
-                if (duration) {
-                    const { min, max } = duration;
-                    const is_valid_duration = input_number >= min && input_number <= max;
-
-                    if (min === max) {
-                        this.error_message = localize(
-                            'Duration value is not allowed. To run the bot, please enter {{min}}.',
-                            { min }
-                        );
-                    } else {
-                        this.error_message = localize(
-                            'Duration value is not allowed. To run the bot, please enter a value between {{min}} to {{max}}.',
-                            { min, max }
-                        );
-                    }
-
-                    return !is_valid_duration;
-                }
-
-                return false;
-            },
+            AMOUNT: input => this.validateAmount(input),
+            DURATION: input => this.validateDuration(input),
         };
     },
 };
