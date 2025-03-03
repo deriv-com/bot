badbac7ec965bc47fdee3335cd663da75722443b
Rupato Braganza <rupato@deriv.com>
2025-03-03 18:48:41 +0800

    fix: currency on first login

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -77,6 +77,8 @@ const Layout = () => {
             !clientHasCurrency
         ) {
             const query_param_currency = sessionStorage.getItem('query_param_currency') || currency || 'USD';
+            console.log('query_param_currency', query_param_currency);
+            sessionStorage.setItem('query_param_currency', currency);
             requestOidcAuthentication({
                 redirectCallbackUri: `${window.location.origin}/callback`,
                 ...(query_param_currency
