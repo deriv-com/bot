a14ee9b73dc9927c2dfa924846c49a8423724ec5
mayuran-deriv <balamayuran@regentmarkets.com>
2025-04-03 14:11:15 +0400

    fix: files restrict

--- src/components/load-modal/local.tsx
+++ src/components/load-modal/local.tsx
@@ -76,7 +76,7 @@ const LocalComponent = observer(() => {
                 <input
                     type='file'
                     ref={file_input_ref}
-                    accept='application/xml, text/xml'
+                    accept='.xml, application/xml, text/xml'
                     style={{ display: 'none' }}
                     onChange={e => setIsFileSupported(handleFileChange(e, false))}
                     data-testid='dt-load-strategy-file-input'
