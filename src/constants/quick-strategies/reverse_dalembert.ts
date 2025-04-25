a028bed5d76210815c5ed2e4c707466133cd921f
moiz-deriv <moiz@regentmarkets.com>
2025-04-25 12:55:39 +0800

    fix: added missing changes

--- src/constants/quick-strategies/reverse_dalembert.ts
+++ src/constants/quick-strategies/reverse_dalembert.ts
@@ -29,7 +29,7 @@ export const REVERSE_D_ALEMBERT = (): TDescriptionItem[] => [
         type: 'text',
         content: [
             localize(
-                '<strong>Initial stake:</strong> The amount that you are willing to place as a stake to enter a trade. This is the starting point for any changes in stake depending on the dynamic of the strategy being used.'
+                '<strong>Initial stake:</strong> The amount that you are willing to place as a stake to enter a trade.'
             ),
         ],
     },
