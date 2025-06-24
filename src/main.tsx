8f11c07b0380e0410fbe607fbbc55963ab744914
Rupato Braganza <rupato@deriv.com>
2025-06-24 13:53:22 +0800

    fix: safari-browser

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import { AuthWrapper } from './app/AuthWrapper';
 import { AnalyticsInitializer } from './utils/analytics';
 import './styles/index.scss';
 
-// Detect Safari browser
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-// Add class to HTML element if Safari
-if (isSafari) {
-    document.documentElement.classList.add('safari-browser');
-}
-
 AnalyticsInitializer();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<AuthWrapper />);
